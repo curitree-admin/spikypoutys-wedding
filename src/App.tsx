208c05392617c09498eabd5607c3bbed47847b5b
curitree-admin <admin@curitree.net>
2025-06-18 19:25:16 +0900

    add: basic details

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Groom from "./pages/Groom";
-import Bride from "./pages/Bride";
+import MainPage from "./pages/MainPage";
 
 const App: React.FC = () => {
   return (
     <div>
       <Routes>
-        <Route path="/groom" element={<Groom/>}/>
-        <Route path="/bride" element={<Bride/>}/>
-        <Route path="*" element={<Bride/>}/>
+        <Route path="*" element={<MainPage/>}/>
       </Routes>
     </div>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
 import data from '../assets/image_data.json';
 import pinIcon from '../assets/location-pin.png';
+import brideAccountData from '../assets/bride_account_number_data.json';
+import groomAccountData from '../assets/groom_account_number_data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container as MapDiv, NaverMap, Marker, useNavermaps} from 'react-naver-maps';
 import '../App.css';
 import ImageModal from '../components/imageModal';
+import AccountModal from '../components/accountModal';
 
-const Groom: React.FC = () => {
+const Bride: React.FC = () => {
   // state for image modal
   const [clickedImg, setClickedImg] = useState<string | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+  // state for account modal
+  const [ clickedAccountData, setClickedAccountData ] = useState<any>(null);
+  const [ copiedAccount, setCopiedAccount ] = useState<string | null>(null);
 
   const navermaps = useNavermaps()
 
   const handleClick = (item: { link: string }, index: number) => {
     setCurrentIndex(index);
     setClickedImg(item.link);
+  };
+  const accountClick = (account_data: { data: any }) => {
+    setClickedAccountData(account_data.data);
   };
 
   const handleRotationRight = () => {
@@ -62,14 +71,14 @@ const Groom: React.FC = () => {
           <div className="col-md">
             <div className='mainsection'>
               <div>
-              <img src="https://dave-khim-aws-bucket-public.s3.ap-northeast-2.amazonaws.com/test/love-tenderness-couple-s-crossed-hands.jpg" className='main-image' alt='t1'></img>
+                <img src="https://dave-khim-aws-bucket-public.s3.ap-northeast-2.amazonaws.com/test/love-tenderness-couple-s-crossed-hands.jpg" className='main-image' alt='t1'></img>
               </div>
               <div className='mainsection-text'>
                 <div className='mainsection-text-1'>결혼식에 초대합니다</div>
                 <div className='mainsection-text-2'>
-                  김신랑 <span className='text2-inner'> & </span> 이신부
+                  최연준 <span className='text2-inner'> & </span> 고은경
                 </div>
-                <div className='mainsection-text-3'>2023. 00. 0 토요일 오전 11시<br/>00대학교 웨딩홀</div>
+                <div className='mainsection-text-3'>2025. 11. 09 일요일 오전 11시<br/>수원 노보텔</div>
               </div>
             </div>
             <div className='invitation-section'>
@@ -82,10 +91,10 @@ const Groom: React.FC = () => {
                     격려해 주시면 감사하겠습니다.
               </div>
               <div className='invitation-section-text3'>
-                김아빠・이엄마<span className='text3-inner'>의 장남</span> 신랑
+                최진태・한미현<span className='text3-inner'>의 차남</span> 최연준
               </div>
               <div className='invitation-section-text3'>
-                이아빠・박엄마<span className='text3-inner'>의 차녀</span> 신부
+                고형균・박명선<span className='text3-inner'>의 차녀</span> 고은경
               </div>
             </div>
             <div className='gallery-section'>
@@ -135,7 +144,7 @@ const Groom: React.FC = () => {
               </MapDiv>
             </div>
             <div className='location-info-section'>
-                <div className='location-info-section-text1'>이라운지 서울대점</div>
+                <div className='location-info-section-text1'>수원 노보텔</div>
                 <div className='location-info-section-text2'>
                     서울특별시 관악구 관악로 1<br/>
                     서울대학교 310동 엔지니어하우스<br/>
@@ -156,6 +165,21 @@ const Groom: React.FC = () => {
                 네비게이션 이용 시 “이라운지 서울대점”을 입력하세요. (주차 2시간 무료)
               </div>
             </div>
+            <div className='congratulatory-section'>
+              <div className='congratulatory-section-text'>마음 전하실 곳</div>
+                <div 
+                  className='congratulatory-section-btn' 
+                  onClick={() => accountClick(groomAccountData)}>신랑측 계좌번호</div>
+                <div 
+                  className='congratulatory-section-btn'
+                  onClick={() => accountClick(brideAccountData)}>신부측 계좌번호</div>
+            </div>
+            {clickedAccountData && <AccountModal 
+              clickedAccountData={clickedAccountData}
+              setClickedAccountData={setClickedAccountData}
+              copiedAccount={copiedAccount}
+              setCopiedAccount={setCopiedAccount}
+              />}
           </div>
 
           <div className="col col-md-2 col-lg-3">
@@ -167,4 +191,4 @@ const Groom: React.FC = () => {
   );
 }
 
-export default Groom;
+export default Bride;
