a25b949a92fda69ad2fe0c6ade78030c179ceadd
curitree-admin <admin@curitree.net>
2025-06-23 22:34:07 +0900

    Default language from browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import { useLanguage } from './i18n';
 
 const App: React.FC = () => {
+  const { language, toggleLanguage } = useLanguage();
   return (
     <div>
+      <button onClick={toggleLanguage} style={{ position: 'absolute', top: 10, right: 10 }}>
+        {language === 'ko' ? 'English' : '한국어'}
+      </button>
       <Routes>
-        <Route path="*" element={<MainPage/>}/>
+        <Route path="*" element={<MainPage />} />
       </Routes>
     </div>
   );
